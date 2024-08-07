f7891a32ed0667ef48d8f9d0e711975c98000e68
rupeshkumar8434 <133231465+rupeshkumar8434@users.noreply.github.com>
2024-08-08 00:01:53 +0530

    URL updated

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,9 +1,9 @@
 
-BASE_URL = 'https://job-portal-backend-j8ji.onrender.com'
-export const USER_API_END_POINT="BASE_URL/api/v1/user";
-export const JOB_API_END_POINT="BASE_URL/api/v1/job";
-export const APPLICATION_API_END_POINT="BASE_URL/api/v1/application";
-export const COMPANY_API_END_POINT="BASE_URL/api/v1/company";
+//BASE_URL = 'https://job-portal-backend-j8ji.onrender.com'
+export const USER_API_END_POINT="https://job-portal-backend-j8ji.onrender.com/api/v1/user";
+export const JOB_API_END_POINT="https://job-portal-backend-j8ji.onrender.com/api/v1/job";
+export const APPLICATION_API_END_POINT="https://job-portal-backend-j8ji.onrender.com/api/v1/application";
+export const COMPANY_API_END_POINT="https://job-portal-backend-j8ji.onrender.com/api/v1/company";
 
 
 // export const USER_API_END_POINT="http://localhost:8000/api/v1/user";
